4f9d28caecdac852d59b6362311259da5be9c966
Dimas Yanu <dimasyanu15@gmail.com>
2024-04-30 17:15:09 +0700

    Updated frontend

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,37 +1,114 @@
 <script setup>
+import { computed, ref, watch } from 'vue';
 import EditorPopup from '@/components/EditorPopup.vue'
 
-const lipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum ex ac mollis suscipit. Suspendisse nec molestie massa. Sed urna enim, dictum eu sodales in, sollicitudin non urna. Curabitur vehicula ligula non metus fermentum tempor. Maecenas turpis orci, cursus interdum bibendum et, consequat sed est. Quisque scelerisque, ante vel posuere maximus, lacus ante fermentum lectus, a ultricies est metus nec diam. In in egestas lorem, quis cursus mi. Proin purus dolor, aliquet consectetur eros non, dignissim posuere leo. Sed id neque diam. In facilisis nunc nisi, eget ultrices massa laoreet a.\n\nSed tempus arcu eget eros commodo lacinia. Aenean vel orci a nisi rutrum ornare. Phasellus pharetra sem eget nibh interdum, in volutpat eros ultrices. Fusce tempor, erat vitae pretium blandit, turpis mi malesuada tortor, in dictum arcu velit non ante. Duis et purus facilisis, porta tortor quis, iaculis diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus placerat suscipit. Duis neque augue, faucibus a erat a, interdum egestas turpis. Ut at auctor lorem.\n\nVestibulum accumsan elit ut lectus sagittis dignissim. Suspendisse sit amet dapibus dui, aliquet volutpat urna. Phasellus quis dolor quis dui pretium imperdiet. Phasellus commodo leo quam, non consectetur justo mattis sit amet. Fusce tempus imperdiet odio, vitae cursus sem. Suspendisse ultrices risus eu mauris ultricies, eget scelerisque nisl vulputate. Vivamus ultricies dolor ut elementum accumsan. Vivamus molestie justo nunc, et euismod mi lacinia non.\n\nPhasellus ultricies interdum urna, vitae sodales metus semper vel. Etiam odio purus, placerat in nibh et, cursus interdum odio. Maecenas egestas dolor quis ipsum viverra malesuada. Suspendisse venenatis molestie maximus. Duis sagittis, neque sed interdum sagittis, turpis magna mattis purus, et sodales eros dui vel dui. Nunc vitae ligula in tortor dapibus laoreet eget ut turpis. Ut pellentesque tristique feugiat. Quisque fringilla quam ac ligula mattis, varius tincidunt tortor pharetra. Nullam bibendum nisl eget sapien faucibus, at consequat ipsum ornare. Phasellus leo nulla, mollis eu tristique nec, bibendum et lacus. Phasellus pellentesque nibh ac aliquam euismod. Ut vestibulum velit a laoreet porttitor. Vivamus odio tortor, vehicula ut scelerisque ac, pellentesque id elit. Fusce enim quam, rhoncus ac eros vel, blandit dapibus tortor.\n\nVivamus ultricies sagittis erat ut rutrum. Aenean egestas dignissim lectus. Nunc varius, nunc quis pellentesque faucibus, odio lectus porttitor ipsum, nec dictum magna justo et sapien. Nulla facilisi. Duis vel lobortis urna. Suspendisse rhoncus mi ac eros ultrices pharetra. Aliquam eget erat eget purus blandit sagittis. Vivamus placerat erat justo, sit amet fermentum felis elementum in. Sed sed sapien eget justo vulputate sagittis in ut sem. Duis viverra tortor eu convallis accumsan. Ut ut lectus non ante vestibulum ullamcorper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean volutpat metus sit amet libero consequat interdum. Phasellus quis accumsan massa.'
+const state = ref({
+  current: {
+    id: null,
+    key: null,
+    description: null
+  },
+  keyword: '',
+  items: [
+    {
+      id: '4b29ba3f-4904-445e-8b6b-96539f3f457d',
+      key: 'Lorem ipsum',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam cursus mi non semper cursus. Nullam sed elit vitae dui condimentum feugiat. Vivamus tincidunt sit amet augue vitae dapibus. Quisque eget ante urna. Cras cursus a nibh in interdum. In hac habitasse platea dictumst. Suspendisse sit amet nisl dignissim, viverra lectus ac, hendrerit nisi. Fusce quis egestas libero. Aliquam urna quam, vulputate non maximus sit amet, eleifend id eros. Suspendisse vitae sagittis quam. Mauris non nulla enim. Vivamus leo risus, tincidunt non ligula nec, ultricies cursus odio. In hac habitasse platea dictumst. Fusce vel tortor lorem. Integer id mattis ipsum. Suspendisse vel lobortis turpis.'
+    },
+    {
+      id: 'a148e645-dcfa-4153-9401-4f6bb03ccb08',
+      key: 'Integer pulvinar',
+      description: 'Integer pulvinar laoreet mi sit amet pretium. Suspendisse blandit fringilla tortor nec rutrum. Curabitur eu leo tellus. Quisque tincidunt cursus diam ac aliquet. Quisque tempus, nisi sit amet vestibulum efficitur, nisi sem ullamcorper eros, nec aliquam diam est vitae massa. Suspendisse sollicitudin, ipsum quis imperdiet bibendum, dolor libero aliquet lorem, ut auctor lectus ante in tortor. Morbi non enim augue. Donec ultricies nisi nec urna consectetur convallis. Duis nibh enim, volutpat id convallis ut, semper nec metus. Aenean egestas turpis ac neque molestie pulvinar.'
+    },
+    {
+      id: 'e54c4aa0-a405-4064-8c3a-bd86c67d389e',
+      key: 'Nullam finibus',
+      description: 'Nullam finibus justo in est posuere dictum eu in ligula. Nunc volutpat consectetur erat, id pretium augue eleifend id. Vestibulum non arcu dapibus, commodo eros sed, feugiat urna. Sed a commodo sapien, congue cursus risus. Proin at odio vel nisl auctor facilisis in nec erat. Suspendisse gravida mattis diam at ultricies. Maecenas eget nibh dui. Maecenas laoreet mauris non elit aliquam euismod. Sed et eros eget lorem efficitur aliquet. Nulla facilisi.'
+    },
+    {
+      id: 'fa4c63c0-cfbd-439e-b602-1bfc760dea18',
+      key: 'Aliquam cursus',
+      description: 'Aliquam cursus ultrices justo, non porttitor tellus aliquam vel. Aenean et efficitur nisi. Nullam ullamcorper purus nec quam gravida, sed consectetur purus pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis efficitur massa quis erat luctus, quis cursus erat porta. Duis egestas ex in arcu rhoncus, at gravida nulla imperdiet. Donec mattis urna in ex varius dapibus. Praesent venenatis laoreet urna sed rutrum. Aenean laoreet erat in gravida rutrum. Etiam eget hendrerit risus, et efficitur ex. Cras hendrerit risus vel orci feugiat auctor. Donec vitae bibendum arcu. Morbi pharetra nisi orci.'
+    },
+    {
+      id: 'fdc3c887-d37b-48b7-88b5-33bb3392c1ff',
+      key: 'Maecenas fringilla',
+      description: 'Maecenas fringilla felis id nunc pharetra tristique. Fusce facilisis, dui sit amet fermentum laoreet, odio lacus cursus dui, vel tristique tellus magna at velit. Ut eu eros fringilla, mollis neque eget, convallis massa. Etiam interdum non nisi sed pulvinar. Fusce placerat ex quis ultrices convallis. Aliquam aliquet scelerisque dolor, vel maximus nisi fermentum in. Ut ut lobortis quam.'
+    }
+  ],
+  showPopup: false
+})
+
+const filteredItems = computed(() => {
+  if (state.value.keyword == null || state.value.keyword === '') return state.value.items
+  return state.value.items.filter(x => x.key.toLowerCase().includes(state.value.keyword.toLowerCase()))
+})
+
+const selectItem = function(id) {
+  state.value.current.id = id
+  let item = state.value.items.find(x => x.id === id)
+  state.value.current.key = item.key
+  state.value.current.description = item.description
+}
+
+const add = function() {
+  state.value.current.id = null
+  state.value.current.key = ''
+  state.value.current.description = ''
+  state.value.showPopup = true
+}
+
+const edit = function() {
+  state.value.showPopup = true
+}
+
+const save = function(payload) {
+  let id = payload.id
+  let key = payload.key
+  let desc = payload.description
+  state.value.showPopup = false
+}
+
+watch(filteredItems, newVal => {
+  state.value.current.id = newVal[0].id
+  state.value.current.key = newVal[0].key
+  state.value.current.description = newVal[0].description
+})
 </script>
 
 <template>
   <main class="font-raleway p-8">
-    <h1 class="text-center mb-8 text-2xl cursor-pointer">My Dictionary</h1>
+    <h1 class="text-center mb-8 text-2xl cursor-pointer" @click="add">My Dictionary</h1>
     <div class="flex flex-col w-[1024px] m-auto border-2 rounded-xl">
 
       <div class="headers w-full flex flex-row">
         <div class="w-[270px] px-4 py-2">
           <input type="text" class="border-[1px] ml-3 rounded-[24px] text-lg px-4 py-2"
-            placeholder="Type to search.." />
+            placeholder="Type to search.." v-model="state.keyword" />
         </div>
-        <div class="grow px-4 py-2 flex items-center">Description</div>
+        <div class="grow px-4 py-2 flex items-center" >Description<span v-if="state.current.id != null" class="cursor-pointer" @click="edit"> - Edit</span></div>
       </div>
 
       <div class="body w-full flex flex-row h-[500px]">
         <div class="w-[270px] grow-0 shrink-0 basis-[270px] px-4 py-2 overflow-y-auto h-full">
           <ul>
-            <li v-for="i in 100" class="px-2 py-1 hover:bg-[#f2f2f2] cursor-pointer">Term {{ i }}</li>
+            <li v-for="item in filteredItems" class="item px-2 py-1 hover:bg-[#f2f2f2] cursor-pointer" :class="{ active: item.id === state.current.id }" @click="selectItem(item.id)">{{ item.key }}</li>
           </ul>
         </div>
         <div class="shrink p-4 overflow-y-auto h-full">
           <div>
-            {{ lipsum }}
+            {{ state.current.description }}
           </div>
         </div>
       </div>
 
     </div>
 
-    <EditorPopup></EditorPopup>
+    <EditorPopup v-if="state.showPopup" 
+      :id="state.current.id"
+      :term="state.current.key"
+      :description="state.current.description"
+      @onSave="save"
+      @onCancel="state.showPopup = false" />
   </main>
 </template>
